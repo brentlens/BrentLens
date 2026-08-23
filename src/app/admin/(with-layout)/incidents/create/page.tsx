c89439d8c21ff7de4ef54189dd4a7bd2bc93ac8c
brentlens <brentlens153@gmail.com>
2026-08-23 09:53:47 +0000

    Fixed admin error

--- src/app/admin/(with-layout)/incidents/create/page.tsx
+++
@@ -1,6 +0,0 @@
-import EditIncidentForm from "@/components/admin/review/EditIncidentForm";
-
-
-export default function CreateIncident() {
-  return <EditIncidentForm isEditing={false} />;
-}
--- src/app/admin/(with-layout)/review/page.tsx
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-import ReviewQueueClient from "@/components/admin/review/ReviewQueueClient";
-import { getAdminIncidents } from "@/services/admin";
-
-export default async function ReviewQueuePage() {
-  const reviewItems = await getAdminIncidents();
-   return <ReviewQueueClient incidents={reviewItems} />;
-}
